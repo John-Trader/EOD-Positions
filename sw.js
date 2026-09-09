35812783d451d1d3569527ee2c101a422d32e6ab
John-Trader <proj4ktor@gmail.com>
2026-09-08 22:35:59 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const SCOPE_URL = new URL('./', self.registration.scope);
 const CACHE_PREFIX = `positioncalc-shell-${encodeURIComponent(SCOPE_URL.pathname)}-`;
-const CACHE_NAME = `${CACHE_PREFIX}v2.2.1`;
+const CACHE_NAME = `${CACHE_PREFIX}v2.5.0`;
 const HTML_URL = new URL('./index.html', SCOPE_URL).href;
 const MANIFEST_URL = new URL('./manifest.webmanifest', SCOPE_URL).href;
 const ICON_URL = new URL('./icon.svg', SCOPE_URL).href;
@@ -118,4 +118,3 @@ self.addEventListener('fetch', event => {
   if (!asset || (asset.html && request.mode !== 'navigate')) return;
   event.respondWith(networkFirstShell(request, asset));
 });
-v
